cc3ca2738e4716512a2788bec4370f7eeda61c24
ibnunadhir200 <adheparwajie12@gmail.com>
2024-09-07 16:47:33 +0700

    iki

---
+++ resources/views/Profile.blade.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PT Putera Adhi Perkasa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+   
+    <nav class="navbar navbar-expand-lg navbar-light border-bottom">
+        <div class="container">
+            <img src="logo.png" alt="PAP Logo" class="img-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="btn btn-dark">Artikel</a>
+                    </li>
+                  
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    
+    <section class="about py-5 border-bottom ">
+        <div class="text-center ">
+            <h1 class="mb-4">Tentang Perusahaan</h1>
+        </div>
+        <div class="container text-left">
+            
+            <div class="row align-items-left ">
+                <div class="col-md-5">
+                    <h2>PT. Putera Adhi Perkasa</h2>
+                    <p>PT. Putera Adhi Perkasa merupakan perusahaan supplier aksesoris bangunan tangan pertama yang telah memiliki brand hardware dan sanitary ternama di Indonesia, diantaranya adalah FLUSSO, VELOCE, PROSPERO, VINCITORY dan MATCH.</p>
+                    <p>Kualitas dan kenyamanan produk untuk konsumen selalu menjadi fokus utama kami, sehingga kami dapat terus berkembang dan telah menjangkau pasar hampir ke seluruh wilayah di Indonesia.</p>
+                    <a href="https://wa.me/6289651502727" class="btn btn-dark">Hubungi Kami</a>
+                </div>
+                <div class="col-md-6 text-left">
+                    <img src="logo.png" alt="PAP Logo" class="img-fluid" align = "right">
+                </div>
+            </div>
+        </div>
+    </section>
+
+   
+    <section class="vision-mission py-5 bg-white">
+        <div class="container">
+            <h2 class="text-left mb-4 ">VISI</h2>
+            <p class="text-left mb-5">"Menjadi Perusahaan Aksesoris Bangunan yang Unggul dan Terkemuka"</p>
+            <h3>MISI</h3>
+            <ul>
+                <li>Mengembangkan bisnis yang berorientasi pada kebutuhan serta kepuasan pelanggan.</li>
+                <li>Menciptakan produk-produk berkualitas yang dinamis dan inovatif dengan harga yang terjangkau.</li>
+                <li>Memperluas jangkauan pemasaran ke seluruh wilayah Indonesia maupun Internasional.</li>
+            </ul>
+            <p><strong>Komitmen Kami:</strong> Putera Adhi Perkasa terbukti mampu menempatkan posisi sebagai salah satu perusahaan yang berkredebilitas tinggi dalam menciptakan produk yang dinamis serta inovatif dan telah disesuaikan dengan standar kenyamanan konsumen di Indonesia.</p>
+            <p>Kesuksesan PT. Putera Adhi Perkasa lahir karena adanya pelayanan optimal yang profesional dan komitmen untuk selalu fokus akan kebutuhan konsumen di Indonesia.</p>
+        </div>
+    </section>
+
+   
+    <footer class="footer bg-dark text-light py-4">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col-md-2 text-left">
+                    <img src="logo.png" alt="PAP Logo" class="img-fluid">
+                </div>
+                <div class="col-md-8 text-center">
+                    <p><a href="https://wa.me/6289651502727" class="text-light">Hubungi Kami</a></p>
+                </div>
+                <div class="col-md-2 text-center">
+                    <p><a href="#" class="text-light">Alamat</a></p>
+                </div>
+             
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('Index');
+});
+Route::get('/', function () {
+    return view('Profile');
+});
